ccb0398f43a92a7cb3dbfa134fdbc75082d05c4c
liwenhui <liwen_hui@163.com>
2018-01-23 19:54:15 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lee on 2018/1/22.
+ */
+module.exports = {
+    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    }
+}
