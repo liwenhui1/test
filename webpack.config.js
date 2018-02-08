6f64cfe4f9d1b0af330ca7755526356ecf3a29e3
liwenhui1 <liwen_hui@163.com>
2018-02-08 23:36:28 +0800

    添加全景cube实现

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 /**
  * Created by lee on 2018/1/22.
  */
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
 module.exports = {
-    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    entry:  __dirname + "/app/main-texture.js",//已多次提及的唯一入口文件
     output: {
         path: __dirname + "/public",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
-    }
+    },
+    plugins: [
+            // copy custom static assets
+            new CopyWebpackPlugin([
+                {
+                    from: path.resolve(__dirname+ "/app/img"),
+                    to: path.resolve(__dirname+ '/public/img')
+                }
+            ])
+    ]
 }
